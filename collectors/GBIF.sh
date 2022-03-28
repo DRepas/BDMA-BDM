a3566863ad431bf56664e467bda841c1c92e3358
DRepas <diogo.repas@gmail.com>
2022-03-28 23:47:59 +0000

    renamed folder structure

--- collectors/GBIF.sh
+++ collectors/GBIF.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+version="v1"
 download_key="0190379-210914110416597" #$(curl -Ss --include --user "diogo.repas.upc:Dade6cVRmB#5eJ" --header "Content-Type: application/json" --data @download_query.json https://api.gbif.org/v1/occurrence/download/request)
 while :
 do 
-    request=$(curl -Ss https://api.gbif.org/v1/occurrence/download/$download_key)
+    request=$(curl -Ss https://api.gbif.org/$version/occurrence/download/$download_key)
     status=$(echo $request | jq .status | tr -d '"')
     echo Download request status is $status.
     if [ $status == "SUCCEEDED" ]
@@ -13,6 +14,6 @@ do
     sleep 5
 done
 download_link=$(echo $request | jq .downloadLink | tr -d '"')
-zipfile="gbif-$download_key.zip"
+zipfile="gbif$version-$download_key.zip"
 echo Downloading $download_link to $zipfile
-curl $download_link | ~/BDM_Software/hadoop/bin/hdfs hdfs dfs -put -f - $zipfile
+curl $download_link | ~/BDM_Software/hadoop/bin/hdfs dfs -put -f - $zipfile
---
+++ collectors/WorldClim.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+version="2.1"
+resolution="30s"
+variables=["tmin","tmax","tavg","prec","srad","wind","vapr","bio","elev"]
+for variable in variables
+do
+    download_link="https://biogeo.ucdavis.edu/data/worldclim/v${version}/base/wc${version}_${resolution}_${variable}.zip"
+    zipfile="wc${version}_${resolution}_${variable}.zip"
+    echo Downloading $download_link to $zipfile
+    curl $download_link | ~/BDM_Software/hadoop/bin/hdfs dfs -put -f - $zipfile
+done
