b0d8957311dc78e7b1be7626a5cd5b74f503e742
DRepas <diogo.repas@gmail.com>
2022-03-29 01:32:22 +0000

    Update collectors

--- collectors/GBIF.sh
+++ collectors/GBIF.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+temporary_landing_zone="/tmp"
+
 version="v1"
-download_key="0190379-210914110416597" #$(curl -Ss --include --user "diogo.repas.upc:Dade6cVRmB#5eJ" --header "Content-Type: application/json" --data @download_query.json https://api.gbif.org/v1/occurrence/download/request)
+download_key="0196579-210914110416597" #$(curl -s --user "diogo.repas.upc:Dade6cVRmB#5eJ" --header "Content-Type: application/json" --data @GBIF_query.json https://api.gbif.org/$version/occurrence/download/request)
+echo Download request key is $download_key.
+
 while :
 do 
-    request=$(curl -Ss https://api.gbif.org/$version/occurrence/download/$download_key)
+    request=$(curl -s https://api.gbif.org/$version/occurrence/download/$download_key)
     status=$(echo $request | jq .status | tr -d '"')
     echo Download request status is $status.
     if [ $status == "SUCCEEDED" ]
@@ -14,6 +18,6 @@ do
     sleep 5
 done
 download_link=$(echo $request | jq .downloadLink | tr -d '"')
-zipfile="gbif$version-$download_key.zip"
+zipfile="$temporary_landing_zone/gbif$version-$download_key.zip"
 echo Downloading $download_link to $zipfile
 curl $download_link -L | ~/BDM_Software/hadoop/bin/hdfs dfs -put -f - $zipfile
