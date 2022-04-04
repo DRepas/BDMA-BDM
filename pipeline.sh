8abc9c1716bd49f07854317b88c2febf65a0f664
DRepas <diogo.repas@gmail.com>
2022-04-04 18:46:33 +0000

    Updated structure and datasets

--- pipeline.sh
+++ pipeline.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Run in dettached tmux sessions (in parallel), allows exiting ssh
-tmux new -d -s GBIF 'cd collectors && python3 GBIF.py && cd ..\loaders && python3 GBIF.py && cd ..'
-tmux new -d -s WorldClim 'cd collectors && python3 WorldClim.py && cd ..\loaders && python3 WorldClim.py && cd ..'
-tmux new -d -s NEO 'cd collectors && python3 NEO.py && cd ..\loaders && python3 NEO.py && cd ..'
+tmux new -d -s GBIF 'cd extractors && python3 GBIF.py && cd ..\loaders && python3 GBIF.py && cd ..'
+tmux new -d -s WorldClim 'cd extractors && python3 WorldClim.py && cd ..\loaders && python3 WorldClim.py && cd ..'
+tmux new -d -s NEO 'cd extractors && python3 NEO.py && cd ..\loaders && python3 NEO.py && cd ..'
