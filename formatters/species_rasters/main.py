28b52c65ca80aded96f9f6da0d68ac5d2378b367
DRepas <diogo.repas@gmail.com>
2022-06-09 17:12:40 +0000

    Format samples' columnsdatatypes

--- formatters/species_rasters/main.py
+++ formatters/species_rasters/main.py
@@ -4,6 +4,7 @@ from types import SimpleNamespace
 
 from hdfs import InsecureClient
 from pyspark.sql import SparkSession
+from pyspark.sql.types import FloatType
 
 # Load configuration
 with open('./config/config.json', 'r') as f:
@@ -24,6 +25,25 @@ spark = (SparkSession
 # Read files from HDFS
 background = spark.read.csv("hdfs://pikachu.fib.upc.es:27000/user/bdm/swd/background.csv", header=True)
 samples = spark.read.csv("hdfs://pikachu.fib.upc.es:27000/user/bdm/swd/samples.csv", header=True)
+
+# Format datatypes
+samples = samples.filter(samples.tmin != 'NaN')
+
+samples = (samples
+           .withColumn("longitude", samples["longitude"].cast(FloatType()))
+           .withColumn("latitude", samples["latitude"].cast(FloatType()))
+           .withColumn("tmin", samples["tmin"].cast(FloatType()))
+           .withColumn("tmax", samples["tmax"].cast(FloatType()))
+           .withColumn("tavg", samples["tavg"].cast(FloatType()))
+           .withColumn("prec", samples["prec"].cast(FloatType()))
+           .withColumn("srad", samples["srad"].cast(FloatType()))
+           .withColumn("wind", samples["wind"].cast(FloatType()))
+           .withColumn("vapr", samples["vapr"].cast(FloatType()))
+           .withColumn("bio", samples["bio"].cast(FloatType()))
+           .withColumn("elev", samples["elev"].cast(FloatType()))
+           .withColumn("SRTM_RAMP2_TOPO", samples["SRTM_RAMP2_TOPO"].cast(FloatType()))
+           .withColumn("MOD_LSTD_M", samples["MOD_LSTD_M"].cast(FloatType()))
+           )
 
 # Save the table in the database (dbtable)
 (background
